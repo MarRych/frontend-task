7ee23064b01f980e2dc7d3556bea0a5bf2ba2c7b
marrych <rycmar38@gmail.com>
2022-08-29 19:42:29 +0200

    inderx, styles and js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//popup message//
+let popup = document.getElementById("popup");
+
+function openPopup(){
+ popup.classList.add("open-popup");
+  color 
+}
+function closePopup(){
+  popup.classList.remove("open-popup"); 
+}
+
+
+
+
+ 
